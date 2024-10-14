3fef043d3b922cbd03515fafcee616af3d40f060
kyujinHan <98331298+KyujinHan@users.noreply.github.com>
2024-10-15 00:20:10 +0900

    Add files via upload

---
+++ korean_eval.sh
@@ -0,0 +1,47 @@
+## CSAT-QA eval
+python lm_eval \
+    --model hf \
+    --model_args pretrained=HumanF-MarkrAI/Gukbap-Gemma2-9B \
+    --tasks csatqa \
+    --device cuda:0 \
+    --batch_size 4 \
+    --num_fewshot 0
+
+## kmmlu_direct eval (5-shot)
+python lm_eval \
+    --model hf \
+    --model_args pretrained=HumanF-MarkrAI/Gukbap-Gemma2-9B \
+    --tasks kmmlu_direct \
+    --device cuda:0 \
+    --batch_size 4 \
+    --num_fewshot 5
+
+## haerae eval
+python lm_eval \
+    --model hf \
+    --model_args pretrained=HumanF-MarkrAI/Gukbap-Gemma2-9B \
+    --tasks haerae \
+    --device cuda:0 \
+    --batch_size 4 \
+    --num_fewshot 0
+
+## Logickor eval (2024.10.14 ver)
+python ./MTBench/mtbench.py \
+    --is_multi_turn 1 \
+    --eval_model gpt-4-1106-preview \
+    --repo_name HumanF-MarkrAI \
+    --base_model Gukbap-Gemma2-9B \
+    --max_token 4096 \
+    --huggingface_token '[...token...]' \
+    --api '[...api...]'
+
+
+## K2-eval
+python ./MTBench/k2_eval.py \
+    --is_multi_turn 0 \
+    --eval_model gpt-4-1106-preview \
+    --repo_name HumanF-MarkrAI \
+    --base_model Gukbap-Gemma2-9B \
+    --max_token 4096 \ 
+    --huggingface_token '[...token...]' \
+    --api '[...api...]'
